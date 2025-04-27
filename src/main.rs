f6935544dddb87fa7208e78e4c2d9750ed014bc4
Sai Sunhith Reddy Nagireddy <sxn220005@utdallas.edu>
2025-04-27 00:21:33 -0500

    initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use server::Server;
+mod server; // mod fileName
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
--- target/debug/deps/httpserver-673f53307b9e43c6.d
+++
@@ -1,5 +0,0 @@
-/Users/saisunhithreddynagireddy/Desktop/Rust/Git-Projects/httpserver/target/debug/deps/libhttpserver-673f53307b9e43c6.rmeta: src/main.rs
-
-/Users/saisunhithreddynagireddy/Desktop/Rust/Git-Projects/httpserver/target/debug/deps/httpserver-673f53307b9e43c6.d: src/main.rs
-
-src/main.rs:
